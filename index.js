b72e2dc5fe3fc125d595e5cc33cee584cbbe2781
Akshay Antony <akshaya@valorem.com>
2020-09-18 23:54:44 +0530

    static assets with express

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.json({
-        name: 'Akshay'
-    });
+    response.sendFile(path.resolve(__dirname, 'index.html'))
 });
 
 app.get('/about', (request, response) => {
-    response.json({
-        name: 'Akshay'
-    });
+    response.sendFile(path.resolve(__dirname,'about.html'))
 });
 
+
+app.get('/contact', (request, response) => {
+    response.sendFile(path.resolve(__dirname,'contact.html'))
+});
 app.listen(3000, () => {
     console.log("App listening on port 3000");
 });
+app.use(express.static('public'))
 
 // const http = require('http');
 
